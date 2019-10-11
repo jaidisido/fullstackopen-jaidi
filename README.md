bc82dfbd72c6dd10564eb92fc10456b6d3cf77c8
Abdel Jaidi <jaidi@amazon.com>
2019-10-11 11:45:14 +0100

    Part2-a Exercises

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Submissions to the FullStackOpen Javascript/Node/GraphQL online class: https://fullstackopen.com/en/
 
 # Progress
-part0: submitted
-part1: submitted
+- part0: submitted
+- part1: submitted
---
+++ part2/coursecontents/src/components/Course.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Course = ({course}) => {
+    return (
+        <>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </>
+      )
+ }
+ 
+const Header = (props) => {
+   return (
+    <h1>{props.course}</h1>
+   )
+}
+
+const Content = ({parts}) => parts.map(part =>
+    <Part
+        key={part.id} 
+        part={part.name} exercises={part.exercises}
+    />
+)
+
+const Part = (props) => {
+    return (
+    <p> {props.part} {props.exercises} </p>
+    )
+}
+
+const Total = (props) => {
+    return (
+      <p> <strong> Number of exercises {props.parts.map(part => part.exercises).reduce((a, b) => a + b, 0)} </strong> </p>
+    )
+}
+
+export default Course
